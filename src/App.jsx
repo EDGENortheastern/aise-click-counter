7cae1d034521ab9d24bb8fe7f91bcc9a207e08f8
Katia <katia.punter@storyshareplatform.com>
2026-05-31 14:13:51 +0100

    adds smoke vitest

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import './App.css'
+
+const App = ()=> {
+  return(
+    <main>
+      <h1>Hello</h1>
+      <h2>kkkkaaa</h2>
+    </main>
+  )
+}
+
+export default App;
