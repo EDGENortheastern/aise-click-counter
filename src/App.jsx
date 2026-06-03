06e76eb11ff8de18b32127fe5997920c35bfe065
Katia <katia.punter@storyshareplatform.com>
2026-06-03 10:50:54 +0100

    passes third test

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from "react"
 import './App.css'
 
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
     <main className="app">
       <h1>Click Counter</h1>
-      <button>
+      <button
+        onClick={() => setCount(count + 1)}
+      >
         Click Me
       </button>
+      <p>Count: {count}</p>
     </main>
   )
 }
